3c54860306b6662fae99023c93d4b3a8185c8e55
EtsushiTakaki <e.takaki.sys24@morijyobi.ac.jp>
2025-07-14 15:10:43 +0900

    2

--- book/models.py
+++ book/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+CATEGORY = (
+    ('business', 'ビジネス'),
+    ('life', '生活'),
+    ('hobby', '趣味'),
+    ('other', 'その他'),
+)
+
+class Shelf(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    category = models.CharField(
+        max_length=100,
+        choices = CATEGORY,
+        )
+
+    def __str__(self):
+        return self.title
--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'book.apps.BookConfig'
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
