5c40c2650ed1fbcf61b6c27b3cd875877dab585c
EtsushiTakaki <e.takaki.sys24@morijyobi.ac.jp>
2025-07-15 14:48:29 +0900

    ほんの一覧ページ、ほんの詳細ページ

--- book/urls.py
+++ book/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    
+    path('book/', views.ListBookView.as_view(), name='list-book'),   
+    path('book/<int:pk>/detail/', views.DetailBookView.as_view(), name='detail-book'), 
 ]
--- book/views.py
+++ book/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Shelf
 
-# Create your views here.
+class ListBookView(generic.ListView):
+    template_name = 'book/book_list.html'
+    model = Shelf
+    context_object_name = 'Shelf'
+
+class DetailBookView(generic.DetailView):
+    template_name = 'book/book_detail.html'
+    model = Shelf
+    context_object_name = 'Shelf'
