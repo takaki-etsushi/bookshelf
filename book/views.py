923ec8c9d62b47d518692c9c28a9ce8f3852e181
EtsushiTakaki <e.takaki.sys24@morijyobi.ac.jp>
2025-07-25 15:13:05 +0900

    レイアウト調整-ヘッダー追加

--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Shelf
+from django.urls import reverse_lazy
 
 class ListBookView(generic.ListView):
     template_name = 'book/book_list.html'
@@ -11,3 +12,25 @@ class DetailBookView(generic.DetailView):
     template_name = 'book/book_detail.html'
     model = Shelf
     context_object_name = 'Shelf'
+    
+class CreateBookView(generic.CreateView):
+    template_name = 'book/book_create.html'
+    model = Shelf
+    context_object_name = 'Shelf'
+    fields = ('title', 'text', 'category')
+    success_url = reverse_lazy('list-book')
+    
+
+class DeleteBookView(generic.DeleteView):
+    template_name = 'book/book_confirm_delete.html'
+    model = Shelf
+    context_object_name = 'Shelf'
+    success_url = reverse_lazy('list-book')
+    
+
+class UpdateBookView(generic.UpdateView):
+    template_name = 'book/book_update.html'
+    model = Shelf
+    context_object_name = 'Shelf'
+    fields = ('title', 'text', 'category')
+    success_url = reverse_lazy('list-book')
